b6fa1ec3ca35d12e07475f083c777c01d127badd
namchip <62062388+namchip@users.noreply.github.com>
2024-03-25 17:56:51 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,11 @@
+function longest_Stirng(arr) {
+    let max = arr[0]
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i].length > max.length) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+let arr = ['nam', 'ki', 'af']
+console.log(longest_Stirng(arr));
